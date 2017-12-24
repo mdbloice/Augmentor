a4a813e150abc1d3bfd9a8afa548071b07f54dfc
Vu Gia Truong <gachiemchiep@gmail.com>
2017-12-24 15:11:02 +0900

    Add NumpyPipeline: generate samples images from a list of numpy array and labels

--- Augmentor/__init__.py
+++ Augmentor/__init__.py
@@ -12,6 +12,7 @@ error, more reproducible, more efficient, and easier to perform.
 """
 
 from .Pipeline import Pipeline
+from .NumpyPipeline import NumpyPipeline
 
 __author__ = """Marcus D. Bloice"""
 __email__ = 'marcus.bloice@medunigraz.at'
